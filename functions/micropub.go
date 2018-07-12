116f98c88305711b5f7bdf01e087d8f17bb554d6
Cole Lyman <cole@colelyman.com>
2018-07-12 00:12:14 -0600

    Fleshed out the micropub endpoint more for debugging

--- functions/micropub.go
+++ functions/micropub.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// a handler for GET requests, used for troubleshooting
+	if request.HTTPMethod == "GET" {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body:       "Everything is working, this is the GET request body: " + request.Body,
+		}, nil
+	}
+	// check if the request is a post
+	if request.HTTPMethod != "POST" {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 405,
+			Body:       "The HTTP method is not allowed, make a POST request",
+		}, errors.New("HTTP method is not valid")
+	}
+	fmt.Println(request.Body)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",
