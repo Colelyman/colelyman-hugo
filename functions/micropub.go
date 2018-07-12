9acaeae429ddbb0e540080f08378aedc39eb3f43
Cole Lyman <cole@colelyman.com>
2018-07-12 02:40:14 -0600

    More debug statements

--- functions/micropub.go
+++ functions/micropub.go
@@ -126,7 +126,8 @@ func checkContentType(req events.APIGatewayProxyRequest) (*events.APIGatewayProx
 					Body:       "Bad Request, error parsing the body of the request",
 				}, errors.New("Error parsing the body of the request")
 			} else if val, ok := bodyValues["h"]; ok && val[0] == "entry" {
-				fmt.Println(val)
+				fmt.Println(val[0])
+				fmt.Println(val[0] == "entry")
 				fmt.Println(bodyValues)
 				return &events.APIGatewayProxyResponse{
 					StatusCode: 400,
