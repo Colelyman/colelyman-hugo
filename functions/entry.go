5c1d554de4cd79b3985f1acd06df5a691f31ce69
Cole Lyman <cole@colelyman.com>
2018-07-12 11:41:10 -0600

    Added some logic to connect to the Github repo

--- functions/entry.go
+++ functions/entry.go
@@ -1,23 +1,59 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/url"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 type Entry struct {
-	Content []string
+	Content string
 	tags    []string
+	slug    string
 }
+
+var ctx context.Context
 
 func CreateEntry(bodyValues url.Values) (string, error) {
 	if _, ok := bodyValues["content"]; ok {
 		fmt.Println(bodyValues)
 		entry := new(Entry)
-		entry.Content = bodyValues["content"]
+		entry.Content = bodyValues["content"][0]
+		entry.tags = bodyValues["category"]
+		entry.slug = bodyValues["mp-slug"][0]
+
+		repo := getRepo(connectGitHub())
+		fmt.Println(repo)
 		return "/", nil
 	}
 	return "",
 		errors.New("Content in response body is missing")
+}
+
+func connectGitHub() *github.Client {
+	ctx = context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.ExpandEnv("$GIT_API_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc)
+}
+
+func getRepo(client *github.Client) *github.Reference {
+	repoURL := strings.Split(os.ExpandEnv("$REPOSITORY_URL"), "/")
+	owner, repoName := repoURL[:2][0], repoURL[:1][0]
+	fmt.Printf("Owner: %s and repoName: %s and branch: %s\n", owner, repoName, os.ExpandEnv("$BRANCH"))
+	repo, _, err := client.Git.GetRef(ctx, owner, repoName, "origin/"+os.ExpandEnv("$BRANCH"))
+	if err != nil {
+		panic(err)
+	}
+
+	return repo
 }
