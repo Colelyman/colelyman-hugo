f67bb63592d40118ac6f0f0c50bcd204b9c9d01e
Cole Lyman <cole@colelyman.com>
2018-07-12 03:52:50 -0600

    Output the value of Me

--- functions/micropub.go
+++ functions/micropub.go
@@ -69,6 +69,7 @@ func checkAccess(token string) (bool, error) {
 
 	// verify results of the response
 	if indieAuthRes.Me != "http:\\/\\/colelyman.com\\/" {
+		fmt.Println(indieAuthRes.Me + " != http:\\/\\/colelyman.com\\/")
 		return false,
 			errors.New("Me does not match")
 	}
